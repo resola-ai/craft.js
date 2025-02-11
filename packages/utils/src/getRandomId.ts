5fc9dc59bebd19d8ab1e079bf5440d1966b3df8f
dao <dao@newlogic.com>
2025-02-11 17:02:20 +0700

    fix: use ulid for node id

--- packages/utils/src/getRandomId.ts
+++ packages/utils/src/getRandomId.ts
@@ -1,4 +1,4 @@
-import { nanoid } from 'nanoid';
+import { ulid } from 'ulid';
 
 // By default nanoid generate an ID with 21 characters. To reduce the footprint, we default to 10 characters.
 // We have a higher probability for collisions, though
@@ -9,4 +9,4 @@ import { nanoid } from 'nanoid';
  * @param size The number of characters that are generated for the ID. Defaults to `10`
  * @returns A random id
  */
-export const getRandomId = (size: number = 10) => nanoid(size);
+export const getRandomId = (size: number = 10) => ulid();
